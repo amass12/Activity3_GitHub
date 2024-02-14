b01332ff0ba80cd260fade995a94fa249b74035f
Alba <a2308920@institutmontilivi.cat>
2024-02-14 20:48:23 +0100

    Update_14.2.24

--- Activity3/Program.cs
+++ Activity3/Program.cs
@@ -103,153 +103,158 @@ namespace Activity3
                 "Who sang \"My Heart Will Go On\" in Titanic?",
                 "Who was the first Spanish director to win an Oscar?"
             };
-            static void ShowReadAnswers(int topicNumber, int questionNum)
+            for (int i = 0; i < questions.Length; i++)
             {
-                String input;
-                if (topicNumber == 1)
+                Console.WriteLine(questions[i]);
+                ShowReadAnswers(topic, i);
+            }
+        }
+
+        static void ShowReadAnswers(int topicNumber, int questionNum)
+        {
+            String input;
+            if (topicNumber == 1)
+            {
+                //SPORTS
+                switch (questionNum)
                 {
-                    //SPORTS
-                    switch (questionNum)
-                    {
-                        case 0:
-                            Console.WriteLine("A.30 minutes");
-                            Console.WriteLine("B.120 minutes");
-                            Console.WriteLine("C.90 minutes");
-                            Console.WriteLine("D.40 minutes");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 1:
-                            Console.WriteLine("A.Butterfly");
-                            Console.WriteLine("B.Crawl");
-                            Console.WriteLine("C.Backstroke");
-                            Console.WriteLine("D.Breaststroke");
-                            CheckAnswersShowResult("b", input = Console.ReadLine());
-                            break;
-                        case 2:
-                            Console.WriteLine("A.Swimming");
-                            Console.WriteLine("B.Tennis");
-                            Console.WriteLine("C.Basketball");
-                            Console.WriteLine("D.Badminton");
-                            CheckAnswersShowResult("b", input = Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.WriteLine("A.4");
-                            Console.WriteLine("B.5");
-                            Console.WriteLine("C.2");
-                            Console.WriteLine("D.3");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 4:
-                            Console.WriteLine("A.England");
-                            Console.WriteLine("B.Argentina");
-                            Console.WriteLine("C.Spain");
-                            Console.WriteLine("D.France");
-                            CheckAnswersShowResult("a", input = Console.ReadLine());
-                            break;
-                        default:
-                            Console.WriteLine("ERROR");
-                            break;
-                    }
-
+                    case 0:
+                        Console.WriteLine("A.30 minutes");
+                        Console.WriteLine("B.120 minutes");
+                        Console.WriteLine("C.90 minutes");
+                        Console.WriteLine("D.40 minutes");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.WriteLine("A.Butterfly");
+                        Console.WriteLine("B.Crawl");
+                        Console.WriteLine("C.Backstroke");
+                        Console.WriteLine("D.Breaststroke");
+                        CheckAnswersShowResult("b", input = Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("A.Swimming");
+                        Console.WriteLine("B.Tennis");
+                        Console.WriteLine("C.Basketball");
+                        Console.WriteLine("D.Badminton");
+                        CheckAnswersShowResult("b", input = Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("A.4");
+                        Console.WriteLine("B.5");
+                        Console.WriteLine("C.2");
+                        Console.WriteLine("D.3");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("A.England");
+                        Console.WriteLine("B.Argentina");
+                        Console.WriteLine("C.Spain");
+                        Console.WriteLine("D.France");
+                        CheckAnswersShowResult("a", input = Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("ERROR");
+                        break;
                 }
-                else if (topicNumber == 2)
+
+            }
+            else if (topicNumber == 2)
+            {
+                //ART
+                switch (questionNum)
                 {
-                    //ART
-                    switch (questionNum)
-                    {
-                        case 0:
-                            Console.WriteLine("A.Buenos Aires");
-                            Console.WriteLine("B.Lyon");
-                            Console.WriteLine("C.London");
-                            Console.WriteLine("D.Paris");
-                            CheckAnswersShowResult("d", input = Console.ReadLine());
-                            break;
-                        case 1:
-                            Console.WriteLine("A.Dalí");
-                            Console.WriteLine("B.Michelangelo");
-                            Console.WriteLine("C.Leonardo Da Vinci");
-                            Console.WriteLine("D.Frida Khalo");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 2:
-                            Console.WriteLine("A.Tizziano");
-                            Console.WriteLine("B.Vincent Van Gogh");
-                            Console.WriteLine("C.Monet");
-                            Console.WriteLine("D.Goya");
-                            CheckAnswersShowResult("b", input = Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.WriteLine("A.Pablo Picasso");
-                            Console.WriteLine("B.Artemisia Gentileschi");
-                            Console.WriteLine("C.Velázquez");
-                            Console.WriteLine("D.Caravaggio");
-                            CheckAnswersShowResult("a", input = Console.ReadLine());
-                            break;
-                        case 4:
-                            Console.WriteLine("A.Giotto");
-                            Console.WriteLine("B.Michelangelo");
-                            Console.WriteLine("C.Leonardo Da Vinci");
-                            Console.WriteLine("D.Donatello");
-                            CheckAnswersShowResult("b", input = Console.ReadLine());
-                            break;
-                        default:
-                            Console.WriteLine("ERROR");
-                            break;
-                    }
+                    case 0:
+                        Console.WriteLine("A.Buenos Aires");
+                        Console.WriteLine("B.Lyon");
+                        Console.WriteLine("C.London");
+                        Console.WriteLine("D.Paris");
+                        CheckAnswersShowResult("d", input = Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.WriteLine("A.Dalí");
+                        Console.WriteLine("B.Michelangelo");
+                        Console.WriteLine("C.Leonardo Da Vinci");
+                        Console.WriteLine("D.Frida Khalo");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("A.Tizziano");
+                        Console.WriteLine("B.Vincent Van Gogh");
+                        Console.WriteLine("C.Monet");
+                        Console.WriteLine("D.Goya");
+                        CheckAnswersShowResult("b", input = Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("A.Pablo Picasso");
+                        Console.WriteLine("B.Artemisia Gentileschi");
+                        Console.WriteLine("C.Velázquez");
+                        Console.WriteLine("D.Caravaggio");
+                        CheckAnswersShowResult("a", input = Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("A.Giotto");
+                        Console.WriteLine("B.Michelangelo");
+                        Console.WriteLine("C.Leonardo Da Vinci");
+                        Console.WriteLine("D.Donatello");
+                        CheckAnswersShowResult("b", input = Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("ERROR");
+                        break;
                 }
-                else if (topicNumber == 3)
+            }
+            else if (topicNumber == 3)
+            {
+                //FILMS
+                switch (questionNum)
                 {
-                    //FILMS
-                    switch (questionNum)
-                    {
-                        case 0:
-                            Console.WriteLine("A.blue");
-                            Console.WriteLine("B.yellow");
-                            Console.WriteLine("C.red");
-                            Console.WriteLine("D.black");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 1:
-                            Console.WriteLine("A.10");
-                            Console.WriteLine("B.6");
-                            Console.WriteLine("C.9");
-                            Console.WriteLine("D.7");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 2:
-                            Console.WriteLine("A.2010");
-                            Console.WriteLine("B.2033");
-                            Console.WriteLine("C.2015");
-                            Console.WriteLine("D.2024");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.WriteLine("A.Mariah Carey");
-                            Console.WriteLine("B.Kate Winslet");
-                            Console.WriteLine("C.Whitney Houston");
-                            Console.WriteLine("D.Celine Dion");
-                            CheckAnswersShowResult("d", input = Console.ReadLine());
-                            break;
-                        case 4:
-                            Console.WriteLine("A.Javier Bardem");
-                            Console.WriteLine("B.Paco León");
-                            Console.WriteLine("C.José Luís Garci");
-                            Console.WriteLine("D.Alejandro Amenabar");
-                            CheckAnswersShowResult("c", input = Console.ReadLine());
-                            break;
-                        default:
-                            Console.WriteLine("ERROR");
-                            break;
-                    }
-
+                    case 0:
+                        Console.WriteLine("A.blue");
+                        Console.WriteLine("B.yellow");
+                        Console.WriteLine("C.red");
+                        Console.WriteLine("D.black");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.WriteLine("A.10");
+                        Console.WriteLine("B.6");
+                        Console.WriteLine("C.9");
+                        Console.WriteLine("D.7");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("A.2010");
+                        Console.WriteLine("B.2033");
+                        Console.WriteLine("C.2015");
+                        Console.WriteLine("D.2024");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("A.Mariah Carey");
+                        Console.WriteLine("B.Kate Winslet");
+                        Console.WriteLine("C.Whitney Houston");
+                        Console.WriteLine("D.Celine Dion");
+                        CheckAnswersShowResult("d", input = Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("A.Javier Bardem");
+                        Console.WriteLine("B.Paco León");
+                        Console.WriteLine("C.José Luís Garci");
+                        Console.WriteLine("D.Alejandro Amenabar");
+                        CheckAnswersShowResult("c", input = Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("ERROR");
+                        break;
                 }
-               
-
-
 
             }
 
-            static void CheckAnswersShowResult(String correctAnswer, String input)
+
+        }
+
+        static void CheckAnswersShowResult(String correctAnswer, String input)
             {
                 if (input.Trim().ToLower() == correctAnswer.ToLower().Trim())
                 {
